642b3fa52b4330be9d9c134bb645dcaa866a2e8b
zman43g <zman3@yandex.ru>
2025-09-09 12:27:31 +0700

    ДЗ 15

--- src/Author.java
+++ src/Author.java
@@ -22,11 +22,22 @@ public class Author {
     public String getAuthorSurname() {
         return authorSurname;
     }
+
+    @Override
     public String toString() {
-        return "Автор " + authorName +" "+ authorSurname;
+        return "Автор " + authorName + " " + authorSurname;
     }
 
+    @Override
+    public boolean equals(Object ot) {
+        if (ot == null || getClass() != ot.getClass()) return false;
+        Author author = (Author) ot;
+        boolean checkNameAndSurname = authorName.equals(authorName) && authorSurname.equals(author.authorSurname);
+        return checkNameAndSurname;
+    }
+
+    @Override
     public int hashCode() {
-        return java.util.Objects.hash(authorName,authorSurname);
+        return java.util.Objects.hash(authorName, authorSurname);
     }
 }
